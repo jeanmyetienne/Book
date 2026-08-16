80ef9eccb104b54bbf8a2f57f419f05edb9d3a76
Jeanmy W. Etienne <jeanmyetienne@gmail.com>
2026-08-16 00:36:33 -0400

    add function to manually events.

--- journal.js
+++ journal.js
@@ -5,3 +5,12 @@ let journal = [
     {events : ["work","ice cream","cauliflower", "lasagna", "touched tree", "pizza", "running", "television"], squirrel : false}, 
     {events : ["weekend", "cycling", "break", "peanuts", "beer"], squirrel : false}, 
 ]
+
+//Function that will add events to the journal list.
+
+function addEntry(events, squirrel) { 
+    journal.push(events, squirrel);
+}
+
+addEntry({events : ["Sleep", "touched tree", "pizza", "break"], squirrel: false});
+console.log(journal)
